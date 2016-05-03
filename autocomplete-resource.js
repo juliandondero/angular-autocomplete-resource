ee9254194cc44e078f79cd7a5e4ba8357735e5bd
Nico Varaschin <nicovaras22@gmail.com>
2016-05-03 11:01:49 -0300

    isrequired toma un objeto

--- autocomplete-resource.js
+++ autocomplete-resource.js
@@ -12,7 +12,7 @@ angular.module('autocomplete-resource',['ui.bootstrap'])
                 idautocomplete: '@',
                 classautocomplete: '@',
                 placeholder: '@',
-                isrequired: '@',
+                isrequired: '=',
                 requiredmsj: '@',
                 model: '=',
                 modelsourcename: '@',
@@ -42,7 +42,6 @@ angular.module('autocomplete-resource',['ui.bootstrap'])
 
             },
             link: function (scope, elem, attrs) {
-
                 if (scope.imgItemPreviewSrcAttrib!=null && scope.imgItemPreviewSrcAttrib!=''){
                     scope.imgItemPreviewSrcAttribParsed = scope.imgItemPreviewSrcAttrib;
                 } else {
@@ -76,6 +75,7 @@ angular.module('autocomplete-resource',['ui.bootstrap'])
                 if (scope.tooltipplacement==null){
                     scope.tooltipplacement="top";
                 }
+
 
                 scope.getTooltip=function(){
                     if (scope.withTooltip){
@@ -172,7 +172,7 @@ angular.module('autocomplete-resource',['ui.bootstrap'])
 
                     if (scope.modelsourcefunction!=null){
                         query_func = service[scope.modelsourcefunction];
-                    } 
+                    }
 
                     scope.searching=true;
                     var query_promise = query_func(params, function (itemsReturned) {
@@ -204,7 +204,7 @@ angular.module('autocomplete-resource',['ui.bootstrap'])
 
                 scope.updateItemList = function(filter) {
 
-                    
+
                     var lastModelFilter =filter;
                     $timeout(function () {
                         if(lastModelFilter==scope.modelfilter)
@@ -255,7 +255,7 @@ angular.module('autocomplete-resource',['ui.bootstrap'])
                                 scope.removeItem(!scope.clearOnBlurParsed);
 
                                 break;
-                            case 8:                                
+                            case 8:
                                 //es backspace
                                 scope.listOpened = undefined;
                                 if (scope.model == undefined)
